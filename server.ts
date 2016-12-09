42af53e8f4b79b596fc884111e796bb6dd9b8c2f
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2016-12-09 17:51:01 +0100

    tweak cors

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { FinalRequestObject, RequestHandler, Request, Response, Header, createServer, Router  } from '../reaserve';
+import { FinalRequestObject, RequestHandler, Request, Response, Header, createServer, Router, CorsOptions } from '../reaserve';
 
 // Routes
 import { routes } from './routes';
@@ -6,7 +6,14 @@ import { routes } from './routes';
 // Policies
 import { policies } from './policies';
 
-createServer(4000, routes, policies, true, '*')
+let cors: CorsOptions = {
+    origins: '*',
+    requestMethods: '*',
+    methods: ['GEt', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    headers: ['Access-Control-Allow-Headers', 'Origin' , 'Accept', 'Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers'] // or array of allowed headers e.g: ['authorization', 'content-type'] 
+}
+
+createServer(4000, routes, policies, true, cors)
 .retry(3)
 .onErrorResumeNext()
 .subscribe((fr: FinalRequestObject) => {
